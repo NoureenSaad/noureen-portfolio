2008617287795cfd0a739c4848265ecfbcb3440f
Noureen saad <noureensaad086@gmail.com>
2025-10-07 15:02:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function openWindow(id) {
+  const win = document.getElementById(id + "-window");
+  if (win) {
+    win.style.display = "block";
+    win.style.zIndex = Date.now();
+    makeDraggable(win);
+  }
+}
+
+function closeWindow(id) {
+  const win = document.getElementById(id + "-window");
+  if (win) win.style.display = "none";
+}
+
+function makeDraggable(el) {
+  const header = el.querySelector(".window-header");
+  let offsetX = 0, offsetY = 0, isDown = false;
+
+  header.addEventListener("mousedown", (e) => {
+    isDown = true;
+    offsetX = e.clientX - el.offsetLeft;
+    offsetY = e.clientY - el.offsetTop;
+    el.style.zIndex = Date.now();
+  });
+
+  document.addEventListener("mouseup", () => isDown = false);
+  document.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    el.style.left = `${e.clientX - offsetX}px`;
+    el.style.top = `${e.clientY - offsetY}px`;
+  });
+}
