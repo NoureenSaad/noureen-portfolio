98435740d7f3d16b5235827aecd2e6fff08f2f8f
Noureen saad <noureensaad086@gmail.com>
2025-10-10 18:48:25 +0300

    certificates and activities windows added

--- script.js
+++ script.js
@@ -30,12 +30,3 @@ function makeDraggable(el) {
     el.style.top = `${e.clientY - offsetY}px`;
   });
 }
-
-// function updateClock() {
-//   const now = new Date();
-//   const hours = now.getHours().toString().padStart(2, '0');
-//   const minutes = now.getMinutes().toString().padStart(2, '0');
-//   document.getElementById('clock').textContent = `${hours}:${minutes}`;
-// }
-// setInterval(updateClock, 1000);
-// updateClock();
