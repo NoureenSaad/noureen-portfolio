a5a6b2689eb9e5d72998e426160e25b5c5c44fbb
Noureen saad <noureensaad086@gmail.com>
2025-10-10 17:10:25 +0300

    font is changed + updating font sizes + adding responsive conditions

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function makeDraggable(el) {
   });
 }
 
-function updateClock() {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  document.getElementById('clock').textContent = `${hours}:${minutes}`;
-}
-setInterval(updateClock, 1000);
-updateClock();
+// function updateClock() {
+//   const now = new Date();
+//   const hours = now.getHours().toString().padStart(2, '0');
+//   const minutes = now.getMinutes().toString().padStart(2, '0');
+//   document.getElementById('clock').textContent = `${hours}:${minutes}`;
+// }
+// setInterval(updateClock, 1000);
+// updateClock();
